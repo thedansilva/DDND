44df8efecfdb1a833dbe1352bbc0fd4e7d8bd1c5
thedansilva <43654534+thedansilva@users.noreply.github.com>
2020-11-10 11:13:56 -0500

    update sync methods
    now, you can send messages from any client and the server will broadcast that message back to all clients. thanks to peter for figuring out how to modify multicastserver to take IN messages from clients, as that was a major source of headache.

--- MulticastClient.java
+++ MulticastClient.java
@@ -2,21 +2,52 @@ import java.io.*;
 import java.net.*;
 import java.util.*;
 
-public class MulticastClient {
+public class MulticastClient implements Runnable {
+    static MulticastSocket socket;
+    public void run() {
+        try{
+            System.out.println("Thread running.");
+                while(true) {
+                    DatagramPacket multiPacket;
+                    InetAddress address = InetAddress.getByName("230.0.0.1");
+                    socket = new MulticastSocket(4446);
+                    socket.joinGroup(address);
+                    byte[] buf = new byte[256];
+                    multiPacket = new DatagramPacket(buf, buf.length);
+                    System.out.println("Waiting for packet.");
+                    socket.receive(multiPacket);
+                    String received = new String(multiPacket.getData(), 0, multiPacket.getLength());
+                    System.out.println("BROADCAST: " + received);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
     public static void main(String[] args) throws IOException {
-        MulticastSocket socket = new MulticastSocket(4446);
-        InetAddress address = InetAddress.getByName("230.0.0.1");
-	    socket.joinGroup(address);
         DatagramPacket packet;
-    	for (int i = 0; i < 100; i++) {
-	        byte[] buf = new byte[256];
-            packet = new DatagramPacket(buf, buf.length);
-            socket.receive(packet);
-            String received = new String(packet.getData(), 0, packet.getLength());
-            System.out.println(received);
-	}
+        InetAddress address = InetAddress.getByName("230.0.0.1");
+        socket = new MulticastSocket(4446);
+	    socket.joinGroup(address);
+        MulticastClient listener = new MulticastClient();
+        Thread thread1 = new Thread(listener);
+        thread1.start();
+        try {
+            Scanner in = new Scanner(System.in);
+            byte[] cli = new byte[256];
+            String test;
+            while(true) {
+                System.out.print("Enter thing: ");
+                test = in.nextLine();
+                cli = test.getBytes();
+                packet = new DatagramPacket(cli, cli.length, address, 4445);
+                socket.send(packet);
+                test = "";
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 	socket.leaveGroup(address);
 	socket.close();
     }
-
+    
 }
--- MulticastServerThread.java
+++ MulticastServerThread.java
@@ -3,26 +3,37 @@ import java.net.*;
 import java.util.*;
 
 public class MulticastServerThread extends Thread {
-    protected DatagramSocket socket = null;
-    protected BufferedReader in = null;
+    MulticastSocket socket = null;
+    BufferedReader in = null;
+    InetAddress group;
     public MulticastServerThread() throws IOException {
         super("MulticastServerThread");
-        socket = new DatagramSocket(4445);
+        socket = new MulticastSocket(4445);
+        group = InetAddress.getByName("230.0.0.1");
+        socket.joinGroup(group);
     }
     public void run() {
-        for (int x = 0; x < 5; x++) {
+        DatagramPacket packet;
             try {
-                byte[] buf = new byte[256];
-                String dString = null;
-                dString = new Date().toString();
-                buf = dString.getBytes();
-                InetAddress group = InetAddress.getByName("230.0.0.1");
-                DatagramPacket packet = new DatagramPacket(buf, buf.length, group, 4446);
-                socket.send(packet);
-            } catch (Exception e) {
+                byte[] empty;
+                while(true) {
+                    empty = new byte[256];
+                    packet = new DatagramPacket(empty, empty.length);
+                    System.out.println("waiting");
+                    socket.receive(packet);
+                    System.out.println("Received: ");
+                    String received = new String(packet.getData(), 0, packet.getLength());
+                    System.out.println(received);
+                    byte[] buf = new byte[256];
+                    String dString = "hello from server";
+                    buf = dString.getBytes();
+                    packet = new DatagramPacket(buf, buf.length, group, 4446);
+                    System.out.println("Broadcasting");
+                    socket.send(packet);
+                }
+            } catch (IOException e) {
                 e.printStackTrace();
             }
-        }
 	socket.close();
     }
 }
